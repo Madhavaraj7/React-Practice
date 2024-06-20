39ab317e856188dc01bd1d3e7f137df2b023691c
Madhavaraj R <raj266864@gmail.com>
2024-06-20 14:21:15 +0530

    LazyLoading

--- ReactLazy/src/App.jsx
+++ ReactLazy/src/App.jsx
@@ -1,14 +1,18 @@
-import Example from "./Example.jsx";
-import MyComponent from "./MyComponent.jsx";
+import React, { lazy, Suspense } from 'react';
+
+const Component1 = lazy(() => import('./Component1'));
+const Component2 = lazy(() => import('./Component2'));
 
 function App() {
   return (
     <>
-      {/* <MyComponent /> */}
-
-      <Example/>
-      
-
+      <h1>Lazy Loading</h1>
+      <Suspense fallback={<div>Component 1 is loading, please wait...</div>}>
+        <Component1 />
+      </Suspense>
+      <Suspense fallback={<div>Component 2 is loading, please wait...</div>}>
+        <Component2 />
+      </Suspense>
     </>
   );
 }
---
+++ ReactLazy/src/Component1.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Component1 = () => {
+  return <div>This is Component 1</div>;
+};
+
+export default Component1;
