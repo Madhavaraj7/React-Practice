467fd75270f9f53c14af36a18205ca9230eecbbe
Madhavaraj R <raj266864@gmail.com>
2024-06-24 10:59:40 +0530

    useNavigate

--- Routing/src/Components/Navbar.jsx
+++ Routing/src/Components/Navbar.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import './Navbar.css'
 
 function Navbar() {
+  const navigate = useNavigate()
+
+  const handleClick = ()=>{
+    //api calls
+    navigate("/")
+  }
   return (
     <nav className='Navbar'>
         <h1>Hello World</h1>
         <ul>
             <Link to="/">Home</Link>
-            <Link to="/users">users</Link>
+            {/* <Link to="/users">users</Link> */}
+            <li navigate={handleClick}>Users</li>
             <Link to="/about">About</Link>
             <Link to="/conduct">Contact</Link>
 
